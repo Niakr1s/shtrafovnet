45dd23b56fb2c1eb345ded98ee340bc865d299c5
Niakr1s <pavel2188@gmail.com>
2021-05-27 18:25:51 +0300

    moved contents of parser.go into companyInfoGetter.go

--- services/companyInfoGetter/companyInfoGetter.go
+++ services/companyInfoGetter/companyInfoGetter.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"net/http"
 	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 type Server struct {
@@ -97,4 +99,58 @@ func getCompanyPageLink(inn string) (string, error) {
 	}
 
 	return pageLink, nil
+}
+
+type CompanyInfo struct {
+	Inn   string
+	Kpp   string
+	Name  string
+	Chief string
+}
+
+func parseCompanyInfoPage(pageReader io.Reader) (CompanyInfo, error) {
+	res := CompanyInfo{}
+
+	doc, err := goquery.NewDocumentFromReader(pageReader)
+	if err != nil {
+		return res, err
+	}
+
+	res.Inn, err = getTextFromFirstNode("#clip_inn", doc)
+	if err != nil {
+		return res, err
+	}
+
+	res.Kpp, err = getTextFromFirstNode("#clip_kpp", doc)
+	if err != nil {
+		return res, err
+	}
+
+	res.Name, err = getTextFromFirstNode(".company-name", doc)
+	if err != nil {
+		return res, err
+	}
+
+	res.Chief, err = getTextFromFirstNode(".founder-item__title", doc)
+	if err != nil {
+		return res, err
+	}
+
+	return res, nil
+}
+
+func getTextFromFirstNode(sel string, doc *goquery.Document) (string, error) {
+	var res string
+
+	s := doc.Find(sel)
+	if s.Length() == 0 {
+		return res, fmt.Errorf("node with selector %s not found", sel)
+	}
+
+	s.EachWithBreak(func(i int, s *goquery.Selection) bool {
+		res = s.Text()
+		return false
+	})
+
+	return strings.TrimSpace(res), nil
 }
--- services/companyInfoGetter/companyInfoGetter_test.go
+++ services/companyInfoGetter/companyInfoGetter_test.go
@@ -1,8 +1,10 @@
 package companyInfoGetter
 
 import (
+	"strings"
 	"testing"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -10,4 +12,28 @@ func Test_GetCompanyPageLink(t *testing.T) {
 	link, err := getCompanyPageLink("7734123457")
 	assert.Nil(t, err)
 	assert.Equal(t, "https://www.rusprofile.ru/id/9019334", link)
+}
+
+func Test_ParseCompanyInfoPage(t *testing.T) {
+	page := `
+<div class="company-name" itemprop="legalName">ОБЩЕСТВО ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТИ ОБЩЕСТВО</div>
+<span class="copy_target" id="clip_inn">12341234</span>
+<span class="copy_target" id="clip_kpp">43214321</span>
+<span class="founder-item__title">Босс</span>
+`
+	info, err := parseCompanyInfoPage(strings.NewReader(page))
+
+	assert.NoError(t, err)
+	assert.Equal(t, CompanyInfo{Inn: "12341234", Kpp: "43214321", Name: "ОБЩЕСТВО ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТИ ОБЩЕСТВО", Chief: "Босс"}, info)
+}
+
+func Test_GetTextFromNode(t *testing.T) {
+	node := `<div class="some_node">Node1</div><div class="some_node">Node2</div>`
+
+	doc, _ := goquery.NewDocumentFromReader(strings.NewReader(node))
+
+	text, err := getTextFromFirstNode(".some_node", doc)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "Node1", text)
 }
